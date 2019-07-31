1cf66dffffa52e93359aca239d1ec24cf58a7578
Julianb12 <julianb1212@gmail.com>
2019-07-31 10:53:16 -0500

    Hello World complete

---
+++ src/state-drills/HelloWorld.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+export default class HelloWorld extends React.Component{
+  state = {
+    who: 'world'
+  };
+
+  render() {
+    return(
+      <div>
+      <p>
+        Hello, {this.state.who}
+      </p>
+      <button onClick= { () => this.setState({who: 'World'})}>World</button>
+      <button onClick= { () => this.setState({who: 'friend'})}>Friend</button>
+      <button onClick= { () => this.setState({who: 'React'})}>React</button>
+    </div>
+    );
+  }
+}
